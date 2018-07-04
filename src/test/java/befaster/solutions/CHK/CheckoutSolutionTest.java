c9877795922c9b97503bb6cd866c75c1651e8dd1
sbx_user1060 <sbx_user1060@ip-10-11-85-206.eu-west-2.compute.internal>
2018-07-04 10:34:21 +0000

    Wed Jul 04 10:34:21 UTC 2018

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 import org.junit.Before;
+import org.junit.Test;
 
 public class CheckoutSolutionTest {
   private CheckoutSolution checkout;
@@ -16,5 +17,10 @@ public class CheckoutSolutionTest {
   @Test
   public void testCheckoutABC() throws Exception {
     assertThat(checkout.checkout("A B C"), equalTo(50 + 30 + 20));
+  }
+
+  @Test
+  public void testCheckoutABCD() throws Exception {
+    assertThat(checkout.checkout("A B C D"), equalTo(50 + 30 + 20 + 15));
   }
 }
