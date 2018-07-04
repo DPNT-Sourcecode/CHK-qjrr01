0127836dcae5aec2fe5250b48b05955db43ef7a4
sbx_user1060 <sbx_user1060@ip-10-11-85-206.eu-west-2.compute.internal>
2018-07-04 10:37:21 +0000

    Wed Jul 04 10:37:21 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,6 +1,13 @@
 package befaster.solutions.CHK;
 
+import java.util.Map;
+
 public class CheckoutSolution {
+
+  private Map<String, Integer> priceMap;
+
+  public CheckoutSolution(Map<String, Integer> priceMap) {}
+
   public Integer checkout(String skus) {
     return 50 + 30 + 20;
   }
