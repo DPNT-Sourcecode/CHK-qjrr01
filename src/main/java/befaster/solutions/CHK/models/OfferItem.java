d6287ac8a20b3c8e5caf769675c9613391cdd7a3
sbx_user1060 <sbx_user1060@ip-10-11-211-234.eu-west-2.compute.internal>
2018-07-23 12:11:53 +0000

    Mon Jul 23 12:11:53 UTC 2018

---
+++ src/main/java/befaster/solutions/CHK/models/OfferItem.java
@@ -0,0 +1,9 @@
+package befaster.solutions.CHK.models;
+
+import java.math.BigDecimal;
+
+public class OfferItem {
+  private String name;
+  private String count;
+  private BigDecimal discount;
+}
