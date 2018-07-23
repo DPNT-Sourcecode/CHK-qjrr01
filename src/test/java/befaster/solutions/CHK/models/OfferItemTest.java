6f8f91efb6c3e6dc15028d6af33c762a6aede55f
sbx_user1060 <sbx_user1060@ip-10-11-211-234.eu-west-2.compute.internal>
2018-07-23 12:29:53 +0000

    Mon Jul 23 12:29:53 UTC 2018

--- src/test/java/befaster/solutions/CHK/models/OfferItemTest.java
+++ src/test/java/befaster/solutions/CHK/models/OfferItemTest.java
@@ -13,21 +13,22 @@ public class OfferItemTest {
 
   @Test
   public void testConstructor() throws Exception {
-    new OfferItem("name", 1, BigDecimal(2));
+    new OfferItem("name", 1, new BigDecimal(2));
   }
 
   @Test
   public void testGetName() throws Exception {
-    assertThat(new OfferItem("item", 1, 3).getName()).isEqualTo("item");
+    assertThat(new OfferItem("item", 1, new BigDecimal(3)).getName()).isEqualTo("item");
   }
 
   @Test
   public void testRequiredCount() throws Exception {
-    assertThat(new OfferItem("item", 1, 3).getRequiredCount()).isEqualTo(1);
+    assertThat(new OfferItem("item", 1, new BigDecimal(3)).getRequiredCount()).isEqualTo(1);
   }
 
   @Test
   public void testGetDiscount() throws Exception {
-    assertThat(new OfferItem("item", 1, 3).getDiscount()).isEqualTo(3);
+    OfferItem offerItem = new OfferItem("item", 1, new BigDecimal(3));
+    assertThat(offerItem.getDiscount()).isEqualTo(3);
   }
 }
