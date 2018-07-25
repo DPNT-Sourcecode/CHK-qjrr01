2f52279fc1cae25f18cff2fb92115b650f4ef701
sbx_user1060 <sbx_user1060@ip-10-11-139-0.eu-west-2.compute.internal>
2018-07-25 23:28:52 +0000

    Wed Jul 25 23:28:52 UTC 2018

--- src/main/java/befaster/solutions/CHK/offers/OfferChainProcessor.java
+++ src/main/java/befaster/solutions/CHK/offers/OfferChainProcessor.java
@@ -16,10 +16,16 @@ public class OfferChainProcessor {
     this.root = root;
   }
 
+  private Integer test(String item) {
+    System.out.println("item in stream " + item);
+    return 2;
+  }
+
   public BigDecimal apply(List<String> skus) {
     Map<String, Long> countMap = CheckoutSolution.countMap(skus);
     Set<String> keySet = countMap.keySet();
     System.out.println("KeySet " + keySet.size());
+    keySet.stream().map(item -> test(item)).reduce(BigDecimal.ZERO, BigDecimal::add);
     Stream<BigDecimal> discounts = keySet.stream().map(item -> root.apply(countMap, item));
     return discounts.reduce(BigDecimal.ZERO, BigDecimal::add);
   }
