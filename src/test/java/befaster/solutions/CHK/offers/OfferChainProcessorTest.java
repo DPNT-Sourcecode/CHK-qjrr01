14d2852d61702d53fae6698d17f75d8059b9156a
sbx_user1060 <sbx_user1060@ip-10-11-139-0.eu-west-2.compute.internal>
2018-07-25 23:04:52 +0000

    Wed Jul 25 23:04:52 UTC 2018

--- src/test/java/befaster/solutions/CHK/offers/OfferChainProcessorTest.java
+++ src/test/java/befaster/solutions/CHK/offers/OfferChainProcessorTest.java
@@ -30,6 +30,22 @@ public class OfferChainProcessorTest {
   }
 
   @Test
+  public void testApplyMultipleOffers() throws Exception {
+    OfferItem a = new OfferItem("A", 3, BigDecimal.valueOf(20));
+    OfferItem b = new OfferItem("B", 2, BigDecimal.valueOf(15));
+    OfferItem c = new OfferItem("C", 4, BigDecimal.valueOf(10));
+    Offer offerA = new SimpleOffer(a);
+    Offer offerB = new SimpleOffer(b);
+    Offer offerC = new SimpleOffer(c);
+    OfferChainProcessor processor = new OfferChainProcessor(offerA);
+    processor.addOffer(offerB);
+    processor.addOffer(offerC);
+    List<String> skus = Arrays.asList("A A B A C C B C C".split(" "));
+    BigDecimal discount = processor.apply(skus);
+    assertThat(discount).isEqualTo(BigDecimal.valueOf(20));
+  }
+
+  @Test
   public void testAddOffer() throws Exception {
     OfferItem a = new OfferItem("A", 3, BigDecimal.valueOf(20));
     OfferItem b = new OfferItem("B", 2, BigDecimal.valueOf(15));
@@ -39,9 +55,16 @@ public class OfferChainProcessorTest {
     Offer offerC = new SimpleOffer(c);
     OfferChainProcessor processor = new OfferChainProcessor(offerA);
     processor.addOffer(offerB);
-    processor.addOffer(offerB);
+    processor.addOffer(offerC);
     assertThat(offerA.hasNext()).isTrue();
     assertThat(offerA.getNext().isPresent()).isTrue();
     assertThat(offerA.getNext().get()).isEqualTo(offerB);
+
+    assertThat(offerB.hasNext()).isTrue();
+    assertThat(offerB.getNext().isPresent()).isTrue();
+    assertThat(offerB.getNext().get()).isEqualTo(offerC);
+
+    assertThat(offerC.hasNext()).isFalse();
+    assertThat(offerC.getNext().isPresent()).isFalse();
   }
 }
